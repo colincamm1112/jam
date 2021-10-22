1d160bc9c333335b28bfe0e7ed973ec03010ad7c
colincamm <92947548+colincamm1112@users.noreply.github.com>
2021-10-22 23:10:11 +0000

    change ball velocity

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 
+import turtle
+
 
 wn = turtle.Screen()
-
 wn.bgcolor("black")
 
-wn.register_shape("cookie.gif")
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("green")
+ball.penup()
+ball.speed(0)
+ball.goto(0, 200)
+ball.dy = -1
+grav = 0.1
+clicks  = 0
+ball.dx = 4
 
-cookie = turtle.Turtle()
-cookie.shape("cookie.gif")
-cookie.speed(0)
 
-clicks = 0
+
 
 pen = turtle.Turtle()
 pen.hideturtle()
 pen.color("white")
 pen.penup()
-pen.goto(0, 400)
-pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
+pen.goto(0,200)
+pen.write(clicks, font=("Courier New", 32, "normal"))
 
-def clicked(x, y):
-    global clicks
-    clicks += 1
-    pen.clear()
-    pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
-    
-cookie.onclick(clicked)
+def point():
+  global clicks 
+  clicks += 1
+  pen.clear()
+  pen.write(clicks, font=("Courier New", 32, "normal"))
+
+
+
+while True:
+  wn.update()
+  ball.dy -= grav
+  ball.sety(ball.ycor() + ball.dy)
+  
+  ball.setx(ball.xcor() + ball.dx)
+
+  if ball.ycor() < -220:
+    ball.dy *= -1
+    point()
+  
+  if ball.xcor() < -300:
+    ball.dx *= -1
+    point()
+  
+  if ball.xcor() > 300:
+    ball.dx *= -1
+    point()
+
+
 
 wn.mainloop()
